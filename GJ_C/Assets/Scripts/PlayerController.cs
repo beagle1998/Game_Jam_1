6932f868cdd965ba1bd559320700a6946c92e9c4
DetectiveBleh <56746643+DetectiveBleh@users.noreply.github.com>
2019-10-19 12:48:01 -0700

    Player Movement Script is completed
    its good.

--- GJ_C/Assets/Scripts/PlayerController.cs
+++ GJ_C/Assets/Scripts/PlayerController.cs
@@ -1,31 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
 
-    public float speed;
-    private Rigidbody2D rb;
-    private Vector2 moveVelocity;
+}
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        facingRight = true;
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        moveVelocity = moveInput.normalized * speed;
-    }
-
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        //Check collision of a "groundcheck" cirle with "checkRadius" size at the player's feet, comparing the collision with what Layer we set as ground.
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        Debug.Log("moveInput"); // prints to a console what value moveInput is / helpful for "looking under the hood"
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+
+
+    }
+
+    private void Update()
+    {
+        if(isGrounded == true)
+        {
+            jumps = extraJumps;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+            }
+
+        }
+    }
+
+    //private void FixedUpdate() //more consistent with physics proccesses , view https://stackoverflow.com/questions/34447682/what-is-the-difference-between-update-fixedupdate-in-unity
+    //{
+    //rb.MovePosition(rb.position + moveInput * Time.fixedDeltaTime);
+    //}
+
+    void Flip() // flips character sprite
+    {
+        // playerScale represents the "transform by scaling" window in the Player's sprite component.
+        facingRight = !facingRight;
+        Vector3 playerScale = transform.localScale;
+        playerScale.x *= -1;
+        transform.localScale = playerScale;
     }
 
 
